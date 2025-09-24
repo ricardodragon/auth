b4dd2bfcb4758db56845fdf77e6c5f1eac9376a3
ricardodragon <ricardoeluego@gmail.com>
2025-09-23 21:10:08 -0300

    teste

--- src/main/java/com/dibros/auth/controller/UsuariosController.java
+++ src/main/java/com/dibros/auth/controller/UsuariosController.java
@@ -7,37 +7,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
-import java.security.Principal;
-
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/usuarios")
-//@Api(value = "Endpoints to manage usuarios")
 public class UsuariosController {
 
     private final UsuarioService usuarioService;
 
     @GetMapping
-//    @ApiOperation(value = "List all available courses", response = UsuarioDTO.class)
     public Mono<UsuarioDTO> getUsuario() {
         return this.usuarioService.getApplicationUserByUsername();
     }
 
     @PostMapping
-//    @ApiOperation(value = "Create an new User", response = UsuarioDTO.class)
     public Mono<UsuarioDTO> cadUsuario(@RequestBody UsuarioPostDTO usuarioPostDTO) {
         return this.usuarioService.post(usuarioPostDTO);
     }
 
     @PostMapping("/email-token")
-//    @ApiOperation(value = "Create an new User", response = UsuarioDTO.class)
     public Mono<String> emailToken(@RequestParam String email) {
         return this.usuarioService.emailToken(email);
     }
 
     @DeleteMapping("/{id}")
-//    @ApiOperation(value = "List all available courses", response = UsuarioDTO[].class)
     public Mono<String> deleteUsuario(@PathVariable Long id) {
         return this.usuarioService.deleteUser(id);
     }
--- src/main/java/com/dibros/auth/repository/UsuarioRepository.java
+++ src/main/java/com/dibros/auth/repository/UsuarioRepository.java
@@ -2,7 +2,6 @@ package com.dibros.auth.repository;
 
 import com.dibros.core.model.Usuario;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
